85e8eea713c8e93fe9e8c5ddb134e132a1e8ffe0
Michael Mior <mmior@uwaterloo.ca>
2018-04-15 20:24:33 -0400

    Add some extra rust binaries

--- rust/install.zsh
+++ rust/install.zsh
@@ -7,10 +7,13 @@ fi
 ~/.cargo/bin/rustup update
 
 function rust_install {
-  [[ -e ~/.cargo/bin/$2 ]] || ~/.cargo/bin/cargo install --quiet $1
+  [[ -e ~/.cargo/bin/$2 ]] || ~/.cargo/bin/rustup run nightly cargo install --quiet $1
 }
 
+rust_install cargo-deb cargo-deb
 rust_install cargo-edit cargo-add
+rust_install cargo-info cargo-info
+rust_install cargo-outdated cargo-outdated
 rust_install hyperfine hyperfine
 rust_install ripgrep rg
 rust_install tokei tokei
