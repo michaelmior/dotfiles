53cada50f264d016bd7ef92d0e56ffcc2eae2254
Michael Mior <mmior@uwaterloo.ca>
2018-05-23 20:12:03 -0400

    Fix ShadowFix install

--- system/install.zsh
+++ system/install.zsh
@@ -2,10 +2,10 @@
 profile_directory=$(find ~/.mozilla/firefox -maxdepth 1 -name '*.default' 2> /dev/null | head -1)
 
 if [ ! -z $profile_directory ]; then
-  latest_url=$(curl -s https://api.github.com/repos/overdodactyl/ShadowFox/releases/latest | jq -r '.assets[] | select(.name == "ShadowFox_updater_linux.sh") | .browser_download_url')
-  wget $latest_url -O $profile_directory/ShadowFox_updater_linux.sh
-  chmod +x $profile_directory/ShadowFox_updater_linux.sh
-  yes Y | $profile_directory/ShadowFox_updater_linux.sh
+  latest_url=$(curl -s https://api.github.com/repos/SrKomodo/shadowfox-updater/releases/latest | jq -r '.assets[] | select(.name == "shadowfox_linux_amd64") | .browser_download_url')
+  wget -q $latest_url -O $profile_directory/shadowfox_linux_amd64
+  chmod +x $profile_directory/shadowfox_linux_amd64
+  yes 0 | TERM= $profile_directory/shadowfox_linux_amd64
 else
   echo "Firefox profile directory not found" > /dev/stderr
   exit 1
