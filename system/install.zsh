f2459e1d1dbc909f4dde04e2552675cae7f90714
Michael Mior <mmior@uwaterloo.ca>
2018-06-04 10:29:13 -0400

    Install ls from even-better-ls

--- system/install.zsh
+++ system/install.zsh
@@ -11,3 +11,21 @@ else
   echo "Firefox profile directory not found" > /dev/stderr
   exit 1
 fi
+
+# Install ls from even-better-ls
+if (which -a ls | grep ~/bin > /dev/null)  then
+else
+  >&2 echo "Installing ls..."
+  pushd system/even-better-ls
+  wget https://ftp.gnu.org/gnu/coreutils/coreutils-8.2.tar.xz
+  tar -xf coreutils-8.2.tar.xz
+  rm coreutils-8.2.tar.xz
+  cd coreutils-8.2/src
+  rm -rf ls.c
+  wget https://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
+  cd ..
+  ./configure > /dev/null
+  make > /dev/null
+  cp src/ls ~/bin
+  popd
+fi
