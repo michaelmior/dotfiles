1b3c72d5edf8db14bad1fbbdcb3546d4736066b1
Michael Mior <mmior@uwaterloo.ca>
2018-06-13 06:43:01 -0400

    Lazy load rvm

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -1,5 +1,8 @@
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+rvm() {
+    source "$HOME/.rvm/scripts/rvm"
+    rvm "$@"
+}
 
 # settings from: https://github.com/jruby/jruby/wiki/Improving-startup-time
 export JRUBY_OPTS="-J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1 -J-noverify"
