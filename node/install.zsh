0f5f74c3111cb685eb2a720b6256d1d4da3fe818
Michael Mior <mmior@uwaterloo.ca>
2018-06-13 06:42:51 -0400

    Remove watch binary installed via node

--- node/install.zsh
+++ node/install.zsh
@@ -7,7 +7,7 @@ n stable
 
 npm set progress=false
 npm install -g yarn --loglevel=warn
-(cd $HOME/.dotfiles/node && yarn)
+(cd $HOME/.dotfiles/node && yarn; rm -f node_modules/.bin/watch)
 npm set progress=true
 
 npm completion > $HOME/.dotfiles/node/completion.zsh
