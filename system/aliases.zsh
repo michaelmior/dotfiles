56edd8da63ec9e25d5a3d4563c37d5edfff6178b
Michael Mior <mmior@cs.rit.edu>
2018-08-25 11:48:33 -0400

    Fix ls colors on macOS

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,14 +1,3 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
-
 man() {
   env \
   LESS_TERMCAP_mb=$(printf "\e[1;31m") \
