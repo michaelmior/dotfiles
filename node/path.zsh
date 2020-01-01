18327c9a1c0a6a5e7c19a8c9e05d51fad6ffe3bc
Michael Mior <mmior@cs.rit.edu>
2020-01-01 18:17:35 -0500

    Fix PATH

--- node/path.zsh
+++ node/path.zsh
@@ -1,2 +1,2 @@
 export PATH="./node_modules/.bin:$HOME/.dotfiles/node/node_modules/.bin:$PATH"
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH="$N_PREFIX/bin:$PATH"
