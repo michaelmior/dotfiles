2da0a77812eabdc69fb5bceab9230bda5589dacd
Michael Mior <mmior@cs.rit.edu>
2020-12-06 12:14:10 -0500

    Update Python versions

--- python/update.zsh
+++ python/update.zsh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
 versions=$(curl -s https://www.python.org/downloads/ | pup '.download-list-widget ol li .release-number text{}' | cut -d' ' -f2 | tr '\n' ' ')
-ver2=$(xargs semver -r '> 2.7.16' -r '< 3.0.0' <<< "$versions" | tail -1)
-ver3=$(xargs semver -r '> 3.7.3' <<< "$versions" | tail -1)
+ver2=$(xargs semver -r '> 2.7.18' -r '< 3.0.0' <<< "$versions" | tail -1)
+ver3=$(xargs semver -r '> 3.9.0' <<< "$versions" | tail -1)
 
 if [ "x$ver2" != "x" ]; then
     echo "Updating Python 2 to $ver2"
-    sed -i "s/2.7.16/$ver2/" python/update.zsh python/install.zsh
+    sed -i "s/2.7.18/$ver2/" python/update.zsh python/install.zsh
 fi
 
 if [ "x$ver3" != "x" ]; then
     echo "Updating Python 3 to $ver3"
-    sed -i "s/3.7.3/$ver3/" python/update.zsh python/install.zsh
+    sed -i "s/3.9.0/$ver3/" python/update.zsh python/install.zsh
 fi
